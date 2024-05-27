94e2f747b872914916ec0bc33484cc78c05e7145
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-27 16:41:51 +0200

    Finish 04

---
+++ 04/app.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import MenuItem from "../03/MenuItem.js";
+
+const root = createRoot(document.querySelector("#root"));
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <Menu />
+      </nav>
+    );
+  }
+}
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <ul>
+        <MenuItem text="home" url="/" />
+        <MenuItem text="kontakt" url="/contact" />
+      </ul>
+    );
+  }
+}
+
+root.render(<Nav />);
