e2fa961668da2ce836307fa13315f57377b97ecf
Typee8 <wojciechjasiak1324@gmail.com>
2024-05-29 11:12:48 +0200

    Use MenuItem in Menu.js instead of Nav.js

--- 05/Menu.js
+++ 05/Menu.js
@@ -1,8 +1,11 @@
 import React from "react";
+import MenuItem from "../03/MenuItem";
 
 const Menu = ({ items }) => {
   const listItems = items.map(item => {
-    return item;
+    return (
+      <MenuItem text = {item.text} url = {item.url}/>
+    )
   })
 
   return (
--- 05/Nav.js
+++ 05/Nav.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Menu from "./Menu.js";
-import MenuItem from "../03/MenuItem.js";
 
 class Nav extends React.Component {
   render() {
     const arr = [
-      <MenuItem text={"strona główna"} url = {'/'}/>,
-      <MenuItem text={"www"} url = {'/www'}/>,
+      {text: "strona główna", url: '/'},
+      {text: "www", url: '/www'}
     ];
 
     return (
